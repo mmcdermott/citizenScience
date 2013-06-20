6f6cdedc0ef4cc8764a8a53f59aa624a3980e242
Matt <mattmcdermott8@gmail.com>
2013-06-19 21:53:39 -0400

    Starting Citizen Science Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitizenScience::Application.config.secret_token = '82d363df6eeb996c569b7e6faf60cf58c65347b7c4ee16069d0a2474ca4c0bf104855658e54b46a929f47baddad826c862691e0949386959391c85be4c47f0f6'
